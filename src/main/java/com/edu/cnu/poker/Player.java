2b36071ed3ddcbb2158a4285e4ba58145f36115d
LeeHeungRok <rhh0208@naver.com>
2017-04-27 22:31:32 +0900

    add Player Constructor

--- src/main/java/com/edu/cnu/poker/Player.java
+++ src/main/java/com/edu/cnu/poker/Player.java
@@ -6,4 +6,10 @@ package com.edu.cnu.poker;
 public class Player {
     private int fund;
     private Hand hand;
+
+    public Player() {
+        this.fund = 0;
+        this.hand = null;
+    }
+    
 }
